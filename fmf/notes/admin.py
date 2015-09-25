f4ddd0d33f1277755dd41587bff93f306b32298d
Tadej Novak <tadej@tano.si>
2015-09-25 22:18:40 +0200

    Minor notes admin improvements

--- fmf/notes/admin.py
+++ fmf/notes/admin.py
@@ -2,20 +2,25 @@ from django.contrib import admin
 from .models import Department, Level, Year, Subject, Note
 
 
+class CommonNotesAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'year', 'level', 'department')
     list_filter = ['department', 'level', 'year']
     search_fields = ['name', 'year', 'level', 'department']
+    ordering = ('name', 'department', 'level', 'year')
 
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'subject')
+    list_display = ('id', 'title', 'subject', 'time')
     list_filter = ['time']
     search_fields = ['title', 'description', 'author', 'uploader']
 
 
-admin.site.register(Department)
-admin.site.register(Level)
-admin.site.register(Year)
+admin.site.register(Department, CommonNotesAdmin)
+admin.site.register(Level, CommonNotesAdmin)
+admin.site.register(Year, CommonNotesAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Note, NoteAdmin)

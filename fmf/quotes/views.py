92cec8b3680bd6e8e4ecbb34900d5ba7e111e2b8
Tadej Novak <tadej@tano.si>
2015-10-03 20:48:59 +0200

    Add latest + recent view for quotes

--- fmf/quotes/views.py
+++ fmf/quotes/views.py
@@ -1,18 +1,21 @@
+from random import choice
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 from .models import Quote
 from .pagination import QuotePagination
 from .serializers import QuoteSerializer
 
 
 class QuoteViewSet(viewsets.ModelViewSet):
+    queryset = Quote.objects.all().order_by('-pk')
     serializer_class = QuoteSerializer
     pagination_class = QuotePagination
 
-    def get_queryset(self):
-        params = dict(self.request.GET)
-        if 'random' in params.keys() and 'true' in params['random']:
-            sort = '?'
-        else:
-            sort = '-pk'
+    @list_route()
+    def latest_and_random(self, request):
+        latest = Quote.objects.latest('pk')
+        random = choice(Quote.objects.all())
 
-        return Quote.objects.all().order_by(sort)
+        serializer = self.get_serializer([latest, random], many=True)
+        return Response(serializer.data)

a045c553141ec6afeb5849cb95f9196e39bdd92a
Tadej Novak <tadej@tano.si>
2016-02-27 10:16:57 +0100

    Add basic tests support

--- fmf/quotes/tests.py
+++ fmf/quotes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Quote
 
-# Create your tests here.
+
+class QuoteTestCase(TestCase):
+
+    def setUp(self):
+        Quote.objects.create(author="Janez Novak", content="Bla bla")
+
+    def test_quote_str(self):
+        """Quote string output test"""
+        quote = Quote.objects.get(author="Janez Novak")
+        self.assertEqual(quote.__str__(), "Janez Novak: Bla bla")
